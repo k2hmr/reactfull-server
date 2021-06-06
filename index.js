15a24de1c02992c0a9bb448e256bb7a429927c15
kajimura <k2hmr94@gmail.com>
2021-06-06 13:11:57 +0900

    投稿機能・コメント機能・ログイン機能を追加

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ app.use(express.json());
 app.use(cors());
 
 const db = require("./models");
-const { post } = require("./routes/Posts");
 
 //Routers
 const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
+const commentsRouter = require("./routes/Comments");
+app.use("/comments", commentsRouter);
+const usersRouter = require("./routes/Users");
+app.use("/auth", usersRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
