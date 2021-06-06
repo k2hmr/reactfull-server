71012248e24ad6616133c9af0cd20deb85dab71e
kajimura <k2hmr94@gmail.com>
2021-06-06 16:59:23 +0900

    ブラウザのコンソールでlocalStrege.setItem("accessToken", "...")でログインできないようにする、フロント側からの認証機能を追加 ログインしたらログインボタンが消える機能を追加

--- routes/Users.js
+++ routes/Users.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { Users } = require("../models");
 const bcrypt = require("bcrypt");
-
+const { validateToken } = require('../middlewares/AuthMiddleware');
 const {sign} = require("jsonwebtoken");
-
 
 router.post("/", async (req, res) => {
   const { username, password } = req.body;
@@ -22,18 +21,21 @@ router.post("/login", async (req, res) => {
 
   const user = await Users.findOne({ where: { username: username }});
 
-  if(!user) res.json({error: "User Doesn't Exist"});
+  if(!user) res.json({ error: "User Doesn't Exist" });
 
-  bcrypt.compare(password, user.password).then((match) => {
+  bcrypt.compare(password, user.password).then(async (match) => {
     if(!match) res.json({ error: "Wrong Username And Password Combination" });
 
     const accessToken = sign(
       { username: user.username, id: user.id },
       "importantsecret"
     );
-
     res.json(accessToken);
   });
+});
+
+router.get('/auth', validateToken, (req, res) =>{
+  res.json(req.user)
 });
 
 module.exports = router;
