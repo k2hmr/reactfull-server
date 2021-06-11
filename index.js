8f8c59004bcf1df59d3ae101808b0e77f18b6927
kajimura <k2hmr94@gmail.com>
2021-06-12 01:44:55 +0900

    Fixed all bugs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
@@ -17,8 +18,13 @@ app.use("/auth", usersRouter);
 const likesRouter = require("./routes/Likes");
 app.use("/likes", likesRouter);
 
-db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
-    console.log("Server running on port 3001");
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(process.env.PORT || 3001, () => {
+      console.log("Server running on port 3001");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
   });
-});
